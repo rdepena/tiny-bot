865cee6930b00c1432ee4d7fad6b872626ba5508
Ricardo De Peña <ricardo@isnull.co>
2024-08-01 22:01:04 -0400

    arduino sketch done-ish

---
+++ ino/ultrasonic-sensor/ultrasonic-sensor.ino
@@ -0,0 +1,36 @@
+/*
+  use a HC-SR04 ultrasonic module 
+ * Wiring: Ultrasonic Sensor -> Arduino:
+ * - VCC  -> 5VDC
+ * - TRIG -> Pin 9
+ * - ECHO -> Pin 8
+ * - GND  -> GND
+ */
+
+//Speed of sound to CM conversion
+const float SOS_CM = 0.017;
+
+int trigPin = 9;
+int echoPin = 8;
+
+float ping () {
+  // print the value to Serial Monitor
+  //10 micro-second pulse
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  return SOS_CM * pulseIn(echoPin, HIGH);
+}
+
+void setup() {
+  // begin serial port
+  Serial.begin (9600);
+
+  pinMode(trigPin, OUTPUT);  
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  Serial.println(ping());
+  delay(500);
+}
