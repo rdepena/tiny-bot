2e827355fbf30776af1a3af11bdda8ca13e26e4b
Ricardo De Peña <ricardo@isnull.co>
2025-03-24 19:58:50 -0400

    fixed arduino discovery for the UNO model

--- src/find_arduino.c
+++ src/find_arduino.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-//TODO: Auto-detect the port name by OS
-#define ARDUINO_PREFIX "/dev/cu.usbserial-"  // Modify for linux:  "/dev/ttyUSB" 
-
 char* find_arduino() {
     static char portname[128];
     struct dirent *entry;
@@ -18,6 +15,7 @@ char* find_arduino() {
     while ((entry = readdir(dp))) {
         if (strncmp(entry->d_name, "ttyUSB", 6) == 0 ||  // Linux
             strncmp(entry->d_name, "ttyACM", 6) == 0 ||  // Linux
+            strncmp(entry->d_name, "cu.usbmodem", 11) == 0 ||  // macOS
             strncmp(entry->d_name, "cu.usbserial", 12) == 0) {  // macOS
             snprintf(portname, sizeof(portname), "/dev/%s", entry->d_name);
             closedir(dp);
